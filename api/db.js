c33edaabc659a7c5cbf4eac405b72847b7080540
Douglas Costa <114622378+dbcfilho@users.noreply.github.com>
2024-11-04 13:35:54 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "nsumbu",
-    database: "crud"
+    password: "", // inserir senha do bd
+    database: "" // inserir nome do bd
 })
